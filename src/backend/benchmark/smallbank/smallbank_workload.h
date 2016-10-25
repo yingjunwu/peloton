9a36fb0b6276beec0e002a8c1f4289704c82f729
Yingjun <yingjun@comp.nus.edu.sg>
2016-10-25 19:37:42 +0800

    working on smallbank

--- src/backend/benchmark/smallbank/smallbank_workload.h
+++ src/backend/benchmark/smallbank/smallbank_workload.h
@@ -26,7 +26,6 @@
 #include "backend/executor/delete_executor.h"
 #include "backend/executor/index_scan_executor.h"
 #include "backend/executor/insert_executor.h"
-#include "backend/concurrency/transaction_scheduler.h"
 
 namespace peloton {
 
