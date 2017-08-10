e292a9ad7f4f5d6abc20cbcbffdc519ba4afe665
paulo <pauloamora1111@gmail.com>
2017-08-10 22:20:06 +0000

    Adding serialize tests

--- src/concurrency/timestamp_ordering_transaction_manager.cpp
+++ src/concurrency/timestamp_ordering_transaction_manager.cpp
@@ -752,11 +752,11 @@ ResultType TimestampOrderingTransactionManager::CommitTransaction(
   //////////////////////////////////////////////////////////
 
   auto &manager = catalog::Manager::GetInstance();
-  auto &log_manager = logging::ReorderedPhyLogLogManager::GetInstance();
+  //auto &log_manager = logging::ReorderedPhyLogLogManager::GetInstance();
 
   //log_manager.StartLogging();
-  log_manager.RegisterWorker(current_txn->GetThreadId());
-  log_manager.StartTxn(current_txn);
+  //log_manager.RegisterWorker(current_txn->GetThreadId());
+  //log_manager.StartTxn(current_txn);
 
   // generate transaction id.
   cid_t end_commit_id = current_txn->GetCommitId();
@@ -833,7 +833,7 @@ ResultType TimestampOrderingTransactionManager::CommitTransaction(
         gc_set->operator[](tile_group_id)[tuple_slot] =
             GCVersionType::COMMIT_UPDATE;
 
-        log_manager.LogUpdate(new_version);
+    //    log_manager.LogUpdate(new_version);
 
       } else if (tuple_entry.second == RWType::DELETE) {
         ItemPointer new_version =
@@ -866,7 +866,7 @@ ResultType TimestampOrderingTransactionManager::CommitTransaction(
         gc_set->operator[](tile_group_id)[tuple_slot] =
             GCVersionType::COMMIT_DELETE;
 
-        log_manager.LogDelete(ItemPointer(tile_group_id, tuple_slot));
+//        log_manager.LogDelete(ItemPointer(tile_group_id, tuple_slot));
 
       } else if (tuple_entry.second == RWType::INSERT) {
         PL_ASSERT(tile_group_header->GetTransactionId(tuple_slot) ==
@@ -882,7 +882,7 @@ ResultType TimestampOrderingTransactionManager::CommitTransaction(
 
         // nothing to be added to gc set.
 
-        log_manager.LogInsert(ItemPointer(tile_group_id, tuple_slot));
+//        log_manager.LogInsert(ItemPointer(tile_group_id, tuple_slot));
 
       } else if (tuple_entry.second == RWType::INS_DEL) {
         PL_ASSERT(tile_group_header->GetTransactionId(tuple_slot) ==
@@ -909,7 +909,7 @@ ResultType TimestampOrderingTransactionManager::CommitTransaction(
   ResultType result = current_txn->GetResult();
 
   //log_manager.LogEnd();
-  log_manager.DeregisterWorker();
+  //log_manager.DeregisterWorker();
   //log_manager.End
 
   EndTransaction(current_txn);
