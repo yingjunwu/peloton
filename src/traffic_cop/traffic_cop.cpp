a73cfe4d7c0230c7779b4329130e56124e0636e7
paulo <pauloamora1111@gmail.com>
2017-12-01 04:41:31 +0000

    rebase fixes'

--- src/traffic_cop/traffic_cop.cpp
+++ src/traffic_cop/traffic_cop.cpp
@@ -251,10 +251,7 @@ void TrafficCop::ExecuteStatementPlanGetResult(
     // COMMIT; statement
     p_status_.m_result = ResultType::QUEUING;
     }
-  } else {
-    // COMMIT; statement
-    p_status_.m_result = ResultType::QUEUING;
-  }
+
 }
 
 void TrafficCop::GetDataTables(
